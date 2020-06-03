05ee2d711de52afc5b86d016d3c473af7a95137e
Tcheutchoua Steve <tcheutchouasteve@gmail.com>
2020-06-03 10:46:28 +0100

    test on date times

--- src/test/java/HelloTest.java
+++ src/test/java/HelloTest.java
@@ -1,4 +1,11 @@
+import java.util.Date;
 
 public class HelloTest {
 
+  public static void main(String[] args) {
+    Date d1 = new Date(1590750701050L);
+    Date d2 = new Date(1590750701049L);
+    System.out.println("First date is " + d1.toString());
+    System.out.println("Second date is " + d2.toString());
+  }
 }
